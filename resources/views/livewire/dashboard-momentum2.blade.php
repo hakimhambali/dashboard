f7d073a3e9c250df1a0e3d10389d63f6b245c5d3
hakimhambali <hakimhambali77@gmail.com>
2022-02-01 22:38:48 +0800

    9 commit 1.2.2022

--- resources/views/livewire/dashboard-momentum2.blade.php
+++ resources/views/livewire/dashboard-momentum2.blade.php
@@ -77,7 +77,7 @@
               <div class="row">
                 <div class="col-8">
                   <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">HR-MANPOWER (Target 2022)</p>
+                    <p class="text-sm mb-0 text-capitalize font-weight-bold">HR-MANPOWER (Recruitment)</p>
                     <h5 class="font-weight-bolder mb-0">
 
                     @if ($digit_achievement2>9) 
